f8f0ea653ee99359c1309a30dd2bdaa2dd475e70
DekReza <66171523+AkbarFahreza@users.noreply.github.com>
2026-01-05 12:39:59 +0700

    Add room card and footer
    Add room card detail and footer, the detail page query params still hard coded

--- app/lib/components/quick-booking.tsx
+++ app/lib/components/quick-booking.tsx
@@ -4,7 +4,7 @@ import BookingOptions from "./booking-options";
 export default function QuickBooking() {
   const userName = "Achmad Akbar fahreza";
   return (
-    <div className="lg:rounded-2xl rounded-[7px] py-5 px-5 lg:py-10 lg:px-10 bg-white z-10 absolute top-[60%] mx-auto left-1/2 -translate-x-1/2 w-full shadow-md lg:w-10/12 flex flex-col gap-12">
+    <div className="lg:rounded-2xl rounded-[7px] py-5 px-5 lg:py-10 lg:px-10 bg-white z-10 lg:absolute lg:top-[60%] mx-auto lg:left-1/2 lg:-translate-x-1/2 w-full shadow-md lg:w-10/12 flex flex-col gap-12">
       <div>
         <h2 className="text-main">
           Good afternoon, <span className="font-bold">{userName}</span>
--- app/lib/datas/dummy-data.ts
+++ app/lib/datas/dummy-data.ts
@@ -9,7 +9,7 @@ export default function DummyData() {
           room_id: "CHR1",
           room_name: "Sagitarius",
           room_price_per_night: 300000,
-          isAvailable: true,
+          isAvailable: false,
           room_facilities: [
             "wifi",
             "1 Double Bed",
@@ -47,12 +47,50 @@ export default function DummyData() {
       id: 2,
       hotel_name: "Caspel Hotel Ahmad Yani",
       city: "Surabaya",
-
       rooms: [
         {
           room_id: "CHAY1",
-          room_name: "Sagitarius",
-          room_price_per_night: 320000,
+          room_name: "Lawang Sewu",
+          room_price_per_night: 220000,
+          isAvailable: true,
+          room_facilities: ["wifi", "1 Double Bed", "bathroom"],
+          image:
+            "https://cf.bstatic.com/xdata/images/hotel/max1024x768/484597216.jpg?k=5bced38a4f5ca72698d506a2b9e1604f45bf6e62de2f8aa707dce2888b4c8a86&o=",
+          room_description: "Deskripsi singkat yg hanya satu kalimat",
+        },
+        {
+          room_id: "CHAY2",
+          room_name: "Galaxy Gaming",
+          room_price_per_night: 600000,
+          isAvailable: true,
+          room_facilities: ["wifi", "1 Double Bed", "bathroom", "PC Gaming"],
+          image:
+            "https://www.kkday.com/en-sg/blog/wp-content/uploads/jp_capsule_735728935-1170x680.jpg",
+          room_description: "Lorem ipsum dolor sit amet aiwjdni ",
+        },
+        {
+          room_id: "CHAY3",
+          room_name: "Godzillaxx",
+          room_price_per_night: 230000,
+          isAvailable: false,
+          room_facilities: ["wifi", "1 Single Bed", "bathroom"],
+          image:
+            "https://www.kkday.com/en-sg/blog/wp-content/uploads/jp_capsule_735728935-1170x680.jpg",
+          room_description:
+            "Lorem ipsum dolor sit amet aiwjdni  ajwnd iawhdiah w dhauiwhduai duiahwdhauid uiahduiahduihaiudhaihduiaw d",
+        },
+      ],
+    },
+    {
+      id: 3,
+      hotel_name: "Caspel Hotel Tunjungan",
+      city: "Surabaya",
+
+      rooms: [
+        {
+          room_id: "CHT1",
+          room_name: "Jarvis Room",
+          room_price_per_night: 120000,
           isAvailable: true,
           room_facilities: [
             "wifi",
@@ -65,20 +103,20 @@ export default function DummyData() {
           room_description: "Deskripsi singkat yg hanya satu kalimat",
         },
         {
-          room_id: "CHAY2",
-          room_name: "Unicorn",
-          room_price_per_night: 210000,
+          room_id: "CHt2",
+          room_name: "Nama random",
+          room_price_per_night: 310000,
           isAvailable: false,
-          room_facilities: ["wifi", "1 Double Bed", "bathroom"],
+          room_facilities: ["wifi", "1 Double Bed", "bathroom", "Water Heater"],
           image:
             "https://www.kkday.com/en-sg/blog/wp-content/uploads/jp_capsule_735728935-1170x680.jpg",
           room_description: "Lorem ipsum dolor sit amet aiwjdni ",
         },
         {
-          room_id: "CHAY3",
-          room_name: "Adalah pokoknya",
-          room_price_per_night: 130000,
-          isAvailable: false,
+          room_id: "CHt3",
+          room_name: "Fly High",
+          room_price_per_night: 170000,
+          isAvailable: true,
           room_facilities: ["wifi", "1 Single Bed", "bathroom"],
           image:
             "https://www.kkday.com/en-sg/blog/wp-content/uploads/jp_capsule_735728935-1170x680.jpg",
